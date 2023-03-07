1ba786455c522847dbe14a2ae61c2bfdf899b519
knutinator <jonatanknut@gmail.com>
2023-03-07 14:27:41 +0000

    Add tests for buttons

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -14,14 +14,17 @@ document.close();
 describe("game object contains corrects keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
-    })
+    });
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
-    })
+    });
     test("playerMoves key exists", () => {
         expect("playerMoves" in game).toBe(true);
-    })
+    });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
-    })
-})
+    });
+    test("choices contains corrects ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
