e282b1badcea1cd4a29a70a2ff4e5e8996efe76e
knutinator <jonatanknut@gmail.com>
2023-03-08 08:19:50 +0000

    Add function to start new game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
 let fs = require("fs");
@@ -26,5 +26,28 @@ describe("game object contains corrects keys", () => {
     });
     test("choices contains corrects ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        /* add data for the tests to work with */
+        game.score = 42; 
+        game.playerMoves = ["button2", "button2"];
+        game.currentGame = ["button2", "button2"];    
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toEqual(0);
+    });
+    test("should clear the currentGame array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
